0501a3c25de96a62f051ce9c5f9c3eb797648091
FunTom <vogeltanz@utb.cz>
2022-02-09 15:22:16 +0100

    Přidejte soubory projektu.

---
+++ Hrdina a drak - streda 14/Hrdina.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hrdina_a_drak___streda_14
+{
+    public class Hrdina
+    {
+        public string Jmeno { get; set; }
+        public double Zdravi { get; set; }
+        public double ZdraviMax { get; set; }
+        public double PoskozeniMax { get; set; }
+        public double ZbrojMax { get; set; }
+
+        public Hrdina(string jmeno, double zdravi, double zdraviMax, double poskozeniMax, double zbrojMax)
+        {
+            Jmeno = jmeno;
+            Zdravi = zdravi;
+            ZdraviMax = zdraviMax;
+            PoskozeniMax = poskozeniMax;
+            ZbrojMax = zbrojMax;
+        }
+
+        public double Utok(Drak oponent)
+        {
+            double hodnotaUtoku = 0;
+
+            Random rnd = new Random();
+            hodnotaUtoku = rnd.NextDouble() * PoskozeniMax;
+            hodnotaUtoku -= oponent.Obrana();
+            this.NastavNuluKdyzJeZaporne(ref hodnotaUtoku);
+            oponent.Zdravi -= hodnotaUtoku;
+
+            return hodnotaUtoku;
+        }
+
+        public double Obrana()
+        {
+            double hodnotaObrany = 0;
+
+            //dodelat
+
+            return hodnotaObrany;
+        }
+
+        private void NastavNuluKdyzJeZaporne(ref double hodnota)
+        {
+            if (hodnota < 0)
+                hodnota = 0;
+        }
+
+        public bool JeZivy()
+        {
+            if (Zdravi > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Hrdina a drak - streda 14/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hrdina_a_drak___streda_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hrdina hrdina = new Hrdina("Geralt", 100, 100, 10, 10);
+            Drak drak = new Drak("Alduin", 100, 100, 11, 10);
+            Arena arena = new Arena(hrdina, drak);
+            arena.Boj();
+        }
+    }
+}
